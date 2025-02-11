b7d304a6598297893477c01f5dbba5797fbc5e0e
dcao6668 <dcao6668@gmail.com>
2025-02-11 12:51:07 -0800

    lasercan code v0.1

--- src/main/java/frc/robot/subsystems/Laser.java
+++ src/main/java/frc/robot/subsystems/Laser.java
@@ -3,14 +3,20 @@ package frc.robot.subsystems;
 import au.grapplerobotics.ConfigurationFailedException;
 import au.grapplerobotics.LaserCan;
 import au.grapplerobotics.interfaces.LaserCanInterface.Measurement;
+import edu.wpi.first.util.sendable.Sendable;
+import edu.wpi.first.util.sendable.SendableBuilder;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Constants;
+import frc.robot.Ports;
 
-public class Laser {
-    public static LaserCan intakeLaser = new LaserCan(Constants.Intake.kIntakeLimitSwitchId);
-    public static LaserCan shooterLaser = new LaserCan(Constants.Intake.kShooterLimitSwitchId);
-    
+public class Laser {    
+    public static LaserCan intakeLaser = new LaserCan(Ports.LaserCanIDCoralBack.getDeviceNumber());
+    public static LaserCan shooterLaser = new LaserCan(Ports.LaserCanIDCoralFront.getDeviceNumber());
+    public static LaserCan algaeShooterLaser = new LaserCan(Ports.LaserCanIDAlgae.getDeviceNumber());
+
 
     public Laser() {}
+
 
     public static double getMeasurementIntake() {
         return intakeLaser.getMeasurement().distance_mm;
@@ -20,15 +26,28 @@ public class Laser {
         return shooterLaser.getMeasurement().distance_mm;
     }
 
-    public static boolean inRangeIntake() {
-        return false;
-        //LaserCan.Measurement measurement = intakeLaser.getMeasurement();
-        //return (measurement != null && measurement.status == LaserCan.LASERCAN_STATUS_VALID_MEASUREMENT);
+    public static double getMeasurementAlgaeShooter() {
+        return algaeShooterLaser.getMeasurement().distance_mm;
     }
 
-    public static boolean inRangeShooter() {
+    public static boolean inRangeIntake() {
         LaserCan.Measurement measurement = intakeLaser.getMeasurement();
         return (measurement != null && measurement.status == LaserCan.LASERCAN_STATUS_VALID_MEASUREMENT);
     }
 
+    public static boolean inRangeShooter() {
+        LaserCan.Measurement measurement = shooterLaser.getMeasurement();
+        return (measurement != null && measurement.status == LaserCan.LASERCAN_STATUS_VALID_MEASUREMENT);
+    }
+
+    public static boolean inRangeAlgaeShooter() {
+        LaserCan.Measurement measurement = algaeShooterLaser.getMeasurement();
+        return (measurement != null && measurement.status == LaserCan.LASERCAN_STATUS_VALID_MEASUREMENT);
+    }
+
+    public static void testLaser() {
+        SmartDashboard.putNumber("Laser/Intake sensor",inRangeIntake()?1:0);
+        SmartDashboard.putNumber("Laser/Shooter sensor",inRangeShooter()?1:0);
+        SmartDashboard.putNumber("Laser/Algae sensor",inRangeAlgaeShooter()?1:0);
+    }
 }
