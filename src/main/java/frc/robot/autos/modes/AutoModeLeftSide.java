bb6179c1da449ec5fd8269e5beeba8a01f714617
Dibavi <liverisdimitri@gmail.com>
2025-02-26 20:47:34 -0800

    updated starting positions

--- src/main/java/frc/robot/autos/modes/AutoModeLeftSide.java
+++ src/main/java/frc/robot/autos/modes/AutoModeLeftSide.java
@@ -2,7 +2,7 @@ package frc.robot.autos.modes;
 
 public class AutoModeLeftSide extends AutoStringAuto {
     public AutoModeLeftSide() {
-        super("Elevator DEF S 4 R 5 l " +
+        super("Elevator DEF S 3 R 5 l " +
                 "Elevator L4 CShoot Wait 1 " +
                 "Elevator DEF " + 
                 "CS 2 Wait 1 " + 
--- src/main/java/frc/robot/autos/modes/AutoModeRightSide.java
+++ src/main/java/frc/robot/autos/modes/AutoModeRightSide.java
@@ -2,7 +2,7 @@ package frc.robot.autos.modes;
 
 public class AutoModeRightSide extends AutoStringAuto {
     public AutoModeRightSide() {
-        super("Elevator DEF S 2 R 1 l "+
+        super("Elevator DEF S 1 R 1 l "+
                 "Elevator L4 CShoot Wait 1 Elevator DEF " + 
                 "CS 1 Wait 1 " + 
                 "Elevator DEF R 1 r " + 
