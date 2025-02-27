88034f635b69e14ad76ff008031a40e7fe919977
Dibavi <liverisdimitri@gmail.com>
2025-02-26 21:04:19 -0800

    added automoderightside

--- src/main/java/frc/robot/autos/modes/AutoModeRightSide.java
+++ src/main/java/frc/robot/autos/modes/AutoModeRightSide.java
@@ -2,11 +2,10 @@ package frc.robot.autos.modes;
 
 public class AutoModeRightSide extends AutoStringAuto {
     public AutoModeRightSide() {
-        super("Elevator DEF S 1 R 1 l "+
-                "Elevator L4 CShoot Wait 1 Elevator DEF " + 
-                "CS 1 Wait 1 " + 
-                "Elevator DEF R 1 r " + 
-                "Elevator L4 CShoot Wait 1 Elevator DEF ");
+        super("Elevator DEF S 1 " +
+                "[ ( R 1 l ) ] [ ( Wait 4 Elevator L4 ) ] CShoot Wait 1 Elevator DEF " + 
+                "CS 1 Wait 1 Elevator DEF " + 
+                "[ ( R 1 r ) ] [ ( Wait 5 Elevator L4 ) ] CShoot Wait 1 Elevator DEF ");
     }
 }
-// Paths used: S2-R1l, R1l-CS1, CS1-R1r
+// Paths used: S1-R1l, R1l-CS1, CS1-R1r
