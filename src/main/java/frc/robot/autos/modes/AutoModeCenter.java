acf893128ee07a444397804c560d915c004fff9f
Victor2275 <gusev0219@gmail.com>
2025-03-01 09:47:20 -0800

    finetuning auto center mode path and reduce wait time so that intake algae does not hit algae on reef

--- src/main/java/frc/robot/autos/modes/AutoModeCenter.java
+++ src/main/java/frc/robot/autos/modes/AutoModeCenter.java
@@ -4,8 +4,8 @@ public class AutoModeCenter extends AutoStringAuto {
     public AutoModeCenter() { 
         super("S 2o S 2 " +  
                 "Elevator DEF [ ( R 6l ) "+
-                "( Wait 2 Elevator L4 ) ] CShoot Wait 1 "+
-                "[ ( R 6c ) ( Wait 2 Elevator A1 ) ] " + 
+                "( Wait 1.5 Elevator L4 ) ] CShoot Wait 1 "+
+                "[ ( R 6c ) ( Wait 0.8 Elevator A1 ) ] " + 
                 "AIntake Wait 1 P 1 AShoot ");
     }
     // Paths used: S2-R6l, R6l-R6c, R6c-CS1, CS1-R1l
