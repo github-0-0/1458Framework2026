938de225410299d74b0ec98d7c774ce2d455c41e
Victor2275 <gusev0219@gmail.com>
2025-03-08 14:45:20 -0800

    Led basic

--- src/main/java/frc/robot/RobotContainer25.java
+++ src/main/java/frc/robot/RobotContainer25.java
@@ -332,8 +332,10 @@ public class RobotContainer25 {
                     Math.sqrt(Math.pow(velocity.dx, 2) + Math.pow(velocity.dy, 2)) < 1
                 ) {
                     xboxController.setRumble(GenericHID.RumbleType.kLeftRumble, 0.5);
+                    m_Led.green();
                 } else {
                     xboxController.setRumble(GenericHID.RumbleType.kLeftRumble, 0);
+                    m_Led.red();
                 }
           
                 // for(int i = 0; i < 4;  i++) {
